c2d22ef1a1b4313fd402f4a00138d399d6f4003a
Alyssa Muriel Llorca <murielllorca1@gmail.com>
2016-12-10 18:20:59 +0800

    Update mini_calc.cpp

--- mini_calc.cpp
+++ mini_calc.cpp
@@ -16,15 +16,15 @@ int aDiv(int n1, int n2){
 int main()
 {
 	int n1, n2;
-	cout << "Enter num 1:";
+	cout << "Enter num 1: ";
 	cin >> n1;
-	cout << "Enter num 2:";
+	cout << "Enter num 2: ";
 	cin >> n2;
 	
 
-	cout << "Sum is:"<<aAdd(n1,n2)<<endl;
-	cout << "Difference is:"<<aSub(n1,n2)<<endl;
-	cout << "Product is:"<<aMul(n1,n2)<<endl;
-	cout << "Quotient is:"<<aDiv(n1,n2)<<endl;
+	cout << "Sum is: "<<aAdd(n1,n2)<<endl;
+	cout << "Difference is: "<<aSub(n1,n2)<<endl;
+	cout << "Product is: "<<aMul(n1,n2)<<endl;
+	cout << "Quotient is: "<<aDiv(n1,n2)<<endl;
 	return 0;
 }
