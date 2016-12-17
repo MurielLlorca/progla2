a044acba5d4471d20fa077ac47ce03ecaa05c9b3
Alyssa Muriel Llorca <murielllorca1@gmail.com>
2016-12-17 17:10:20 +0800

    Update structure.cpp

--- structure.cpp
+++ structure.cpp
@@ -1,25 +1,34 @@
+#include <iostream>
+using namespace std;
 struct Person{
 	int toes;
 	int knees;
 	int shoulder;
 	int head;
 };
+void newLine();
 int main()
 {
-	Person person;
-	cout << "Toes: ";
-	cin >> person.toes;
-	cout << "Knees: ";
-	cin >> person.knees;
-	cout << "Shoulder: ";
-	cin >> person.shoulder;
-	cout << "Head: ";
-	cin >> person.head;
+	Person person[4];
+	int i;
 	
-	cout << person.toes << endl;
-	cout << person.knees << endl;
-	cout << person.shoulder << endl;
-	cout << person.head << endl;
-	
+	for (i=0;i<4;i++)
+	{
+	cout << "toes: ";
+	cin >> person[i].toes;
+	cout << "knees: ";
+	cin >> person[i].knees;
+	cout << "shoulder: ";
+	cin >> person[i].shoulder;
+	cout << "head: ";
+	cin >> person[i].head;
+	}
+	for (i=0;i<4;i++)
+	{
+	cout << "Toes: " << person[i].toes << endl;
+	cout << "Knees: " << person[i].knees << endl;
+	cout << "Shoulder: " << person[i].shoulder << endl;
+	cout << "Head: " << person[i].head << endl;
+	}
 	return 0;
 }
