01950cdda2f6d266f63c668aed59cf4cde4968e9
Alyssa Muriel Llorca <murielllorca1@gmail.com>
2016-12-17 16:49:01 +0800

    Update structure.cpp

--- structure.cpp
+++ structure.cpp
@@ -4,3 +4,22 @@ struct Person{
 	int shoulder;
 	int head;
 };
+int main()
+{
+	Person person;
+	cout << "toes";
+	cin >> person.toes;
+	cout << "knees";
+	cin >> person.knees;
+	cout << "shoulder";
+	cin >> person.shoulder;
+	cout << "head";
+	cin >> person.head;
+	
+	cout << person.toes << endl;
+	cout << person.knees << endl;
+	cout << person.shoulder << endl;
+	cout << person.head << endl;
+	
+	return 0;
+}
