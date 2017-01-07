09931549d1aa5694cfbf418df31db6f5be57569f
Alyssa Muriel Llorca <murielllorca1@gmail.com>
2017-01-07 17:08:05 +0800

    Create file_add.cpp

---
+++ file_add.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    FILE *myFile;
+    myFile = fopen("sample.txt", "r");
+	
+	  int sum = 0;
+    int i;
+    int numberArray[5];
+    for (i = 0; i < 5; i++){
+        fscanf(myFile, "%d", &numberArray[i]);
+    }
+
+    for (i = 0; i < 5; i++){
+        printf("Number is: %d\n\n", numberArray[i]);
+    }
+    
+    for (i = 0; i < 5 ; i++){
+    	sum += numberArray[i]; 
+	}
+	
+	cout << "Sum: " << sum;
+	
+	return 0;
+}
