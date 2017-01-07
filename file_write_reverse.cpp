fd6457036766244b5258ef42ef8e45ee112b939c
Alyssa Muriel Llorca <murielllorca1@gmail.com>
2017-01-07 18:29:27 +0800

    Create file_write_reverse.cpp

---
+++ file_write_reverse.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int main(){
+	int lol[5];
+    FILE *fp;
+    fp = fopen("sample1.txt", "w");
+    if(!fp)
+    {
+    	cout << "Cannot open file.\n";
+	}
+	cout << "Enter 5 number: ";
+	for (int i=0; i<5; ++i)
+	{
+		cin >> lol[i];
+	}
+	for (int i=4; i>=0; i--)
+	{
+		cout << lol[i] <<endl;
+	}
+	fclose(fp);
+	fp = fopen("sample1.txt", "w");
+	for (int i=0; i<5; i++){
+		cout << fp << lol[i];
+	}
+	
+	system ("pause");
+    return 0;
+}
