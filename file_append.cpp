746fa97fe28d9984000864700bc54c3af3dcd430
Alyssa Muriel Llorca <murielllorca1@gmail.com>
2017-01-14 16:06:15 +0800

    Update file_append.cpp

--- file_append.cpp
+++ file_append.cpp
@@ -8,21 +8,14 @@ main(){
     //read file into array
     int i, sum = 0;
     int numberArray[5];
-
     for(int i=0; i<5; i++){
         fscanf(myFile, "%d", &numberArray[i]);
-    }
-    for (i = 0; i < 5; i++){
-        printf("%d\n", numberArray[i]);
+	sum+=numberArray[i];
     }
     fclose(myFile);
     myFile = fopen("sample.txt", "a");
-    for(int i=0; i<5; i++){
-      	 fprintf(myFile, "%d", numberArray[i]);
-		sum=4;
-        printf("%d\n", sum+=numberArray[i]);
+    for (i = 0; i < 5; i++){
+        fprintf(myFile, "%d", numberArray[i]+4);
+    	fprintf(myFile, "\n");
     }
-
 }
-
-// kailangan lumabas yung output sa notepad. not yet finish.
